fe80a363997fab86acd3b6a56effc50757ad13ff
MichaelArthurAnderson <michael.arthur.anderson@gmail.com>
2015-03-22 19:03:05 -0400

    Update exercise2.1.py

--- Chapter2/exercise2.1.py
+++ Chapter2/exercise2.1.py
@@ -7,7 +7,7 @@ import sys
 # x = 5
 # x + 1
 #
-# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only untilize techniques
+# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only utilize techniques
 # introduced up to that point in the text.
 
 5
