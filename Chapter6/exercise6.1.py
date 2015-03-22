5f3aec7971ae5b4d4f91cd3e4646d1ed2117cc59
MichaelArthurAnderson <michael.arthur.anderson@gmail.com>
2015-03-22 19:07:28 -0400

    Update exercise6.1.py

--- Chapter6/exercise6.1.py
+++ Chapter6/exercise6.1.py
@@ -4,7 +4,7 @@ import sys
 # Python for Informatics Exploring Information
 #  Exercise 6.1 Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.
 #
-# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only untilize techniques
+# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only utilize techniques
 # introduced up to that point in the text.
 
 string = str(raw_input('Enter a string to print backwards:\n'))
