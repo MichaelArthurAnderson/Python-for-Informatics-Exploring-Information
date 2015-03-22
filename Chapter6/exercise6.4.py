132362be7b7b80f40a48c73c4d516668f1bc4a20
MichaelArthurAnderson <michael.arthur.anderson@gmail.com>
2015-03-22 19:07:56 -0400

    Update exercise6.4.py

--- Chapter6/exercise6.4.py
+++ Chapter6/exercise6.4.py
@@ -5,7 +5,7 @@ import sys
 #  Exercise 6.4 There is a string method called count that is similar to the function in the previous exercise. Read the documentation of this method at docs.python.
 # org/library/string.html and write an invocation that counts the number of times the letter a occurs in 'banana'.
 #
-# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only untilize techniques
+# Note:  While there may be other ways to complete this exercise, I'm following the book in order.  Each example will only utilize techniques
 # introduced up to that point in the text.
 
 print "a appears in banana " + str('banana'.count('a')) + " times."
